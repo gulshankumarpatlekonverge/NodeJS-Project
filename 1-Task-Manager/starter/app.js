ad4ab0658fe1a22303c2c0e6fe0ac1fa5f080d10
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-12 19:28:52 +0530

    correction in folder

---
+++ 1-Task-Manager/starter/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./public'));
+
+app.get('/hello', (req, res) =>{
+    res.send("Hello World");
+})
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server Listening on Port 3000");
+})
