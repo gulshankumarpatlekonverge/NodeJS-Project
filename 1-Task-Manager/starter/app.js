1c35828e64dbf0a210f06cf0ab9a628a6c13b67d
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-13 15:29:27 +0530

    connection to db using app.js

--- 1-Task-Manager/starter/app.js
+++ 1-Task-Manager/starter/app.js
@@ -1,6 +1,6 @@
-require('./db/connect-db')
 const express = require('express');
 const app = express();
+const { connectDB } = require('./db/connect-db')
 const taskRoutes = require('./routes/tasks');
 
 // middleware
@@ -10,6 +10,18 @@ app.use(express.static('./public'));
 
 app.use('/api/v1/tasks', taskRoutes);
 
-app.listen(3000, ()=>{
-    console.log("Server Listening on Port 3000");
-});
+const port = 300;
+
+const start = async() => {
+    try{
+        await connectDB();
+        app.listen(port, ()=>{
+            console.log(`Server Listening on Port ${port}`);
+        });   
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+start();
