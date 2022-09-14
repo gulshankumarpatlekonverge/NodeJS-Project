4e12960df4c0e274bdd8873bc7227f55af8ae535
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-14 19:42:25 +0530

    limit and skip pagination

--- 2- Store-API/controller/products.js
+++ 2- Store-API/controller/products.js
@@ -1,7 +1,8 @@
 const productData = require('../models/product');
 
 const getAllStaticProduct = async (req, res) =>{
-    const products = await productData.find({}).select('name price');
+    const products = await productData.find({}).sort('-name').select('name price')
+    //.limit(10).skip(5);
     // find({ name: 'vase table', featured: true})
     res.status(200).json({ products, nbHits: products.length })
 }
@@ -33,6 +34,14 @@ const  getAllProduct = async (req, res) =>{
         const fieldsList = fields.split(',').join(' ');
         result = result.select(fieldsList)
     }
+
+    // limit and skip - pagination
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
     const products = await result;
     res.status(200).json({ products , nbHits: products.length })
 }
