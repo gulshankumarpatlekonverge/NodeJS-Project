94817d480b6d9a9659c235845a9093dcef4a2ba6
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-14 19:28:26 +0530

    select method

--- 2- Store-API/controller/products.js
+++ 2- Store-API/controller/products.js
@@ -1,14 +1,14 @@
 const productData = require('../models/product');
 
 const getAllStaticProduct = async (req, res) =>{
-    const products = await productData.find({}).sort('-name price');
+    const products = await productData.find({}).select('name price');
     // find({ name: 'vase table', featured: true})
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const  getAllProduct = async (req, res) =>{
     const queryObject = {};
-    const { featured, company, name, sort } = req.query;
+    const { featured, company, name, sort, fields } = req.query;
 
     if(featured) {
         queryObject.featured = featured === 'true' ? true : false;  
@@ -20,11 +20,18 @@ const  getAllProduct = async (req, res) =>{
         queryObject.name = { $regex: name, $options: 'i'}
     }
     let result = productData.find(queryObject);
+    // sort
     if(sort){
         const sortList = sort.split(',').join(' ');
         result = result.sort(sortList)
     } else{
         result = result.sort('createdAt')
+    }
+
+    // select
+    if(fields){
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList)
     }
     const products = await result;
     res.status(200).json({ products , nbHits: products.length })
