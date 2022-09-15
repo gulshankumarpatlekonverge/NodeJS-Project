d3dbc1d2162f24d46acfa851cba576a1a3054b14
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-15 18:14:48 +0530

    fix

--- 4-Jobs-API/middleware/error-handler.js
+++ 4-Jobs-API/middleware/error-handler.js
@@ -18,13 +18,18 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.statusCode = 400;
   }
 
+  if(err.name === 'CastError'){
+    customError.msg = `No Item Found with Id : ${err.value}`
+    customError.statusCode = 404;
+  }
+
   if(err.code && err.code === 11000){
       customError.msg =  `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
       customError.statusCode = 400;
   }
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
-  // return res.status(customError.statusCode).json({msg: customError.msg});
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
+  return res.status(customError.statusCode).json({msg: customError.msg});
 }
 
 module.exports = errorHandlerMiddleware
