34a4e44ec0a11a16679f33477e4ce077aca8933c
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-15 10:41:10 +0530

    configuring jwt token

--- 3-JWT-Basics/ReadME.md
+++ 3-JWT-Basics/ReadME.md
@@ -1,2 +1,5 @@
 # NodeJS 3-JWT-Basics Project
 
+
+# Links
+    JWT Token: https://jwt.io/introduction
--- 3-JWT-Basics/controller/main.js
+++ 3-JWT-Basics/controller/main.js
@@ -1,18 +1,31 @@
-
-
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const loginPageData = (req, res) => {
-    res.status(200).send("Welcome to loginPageData")
+    const {username, password} = req.body;
+
+    if(!username || !password){
+        throw new CustomAPIError('Please Provide Email and Password', 400);
+    } 
+    const id = new Date().getDate();
+    const token = jwt.sign({
+        id, username
+    }, process.env.JWT_SECRET, {expiresIn: '30d'});
+
+    res.status(200).json({msg: 'User Created.', token});
 }
 
+const dashboardPageData = (req, res) => {
+    console.log(req.headers);
+    const randomNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({msg: `Hello, Gulshan`, secret: `Here is your authorized data, your number is ${randomNumber}`});
+}
+ 
 const registrationPageData = (req, res) => {
     res.status(200).send("Welcome to registrationPageData")
 }
 
-const dashboardPageData = (req, res) => {
-    res.status(200).send("Welcome to dashboardPageData")
-}
- 
 module.exports = {
     loginPageData,
     registrationPageData,
