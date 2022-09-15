130e8a9874eb9d9ec4f4e3da9dec76adff792f8c
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-15 15:05:16 +0530

    jwt implementation

--- 4-Jobs-API/controller/auth.js
+++ 4-Jobs-API/controller/auth.js
@@ -2,17 +2,13 @@ const userData = require('../models/auth');
 const { StatusCodes } = require('http-status-codes');
 const {BadRequest} = require('../errors');
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken')
 
 const registerController = async (req, res) => {
-    const { name, email, password } = req.body;
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const tempUser = { name, email, password: hashedPassword};
-    const user = await userData.create({...tempUser})
-    res.status(StatusCodes.CREATED).json({ user })
+    
+    const user = await userData.create({...req.body})
+    const token = user.createJWT();
+    res.status(StatusCodes.CREATED).json({ user:{ user: user.name }, token })
 }
 
 const loginController = (req, res) => {
--- 4-Jobs-API/models/auth.js
+++ 4-Jobs-API/models/auth.js
@@ -1,5 +1,9 @@
 const { string } = require('joi');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const { JsonWebTokenError } = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -19,5 +23,17 @@ const UserSchema = new mongoose.Schema({
         required: [true, 'Please Provide Password.']
     }
 })
+
+
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+    // in mongoose 5.x and later you can remove next
+})
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({userId: this._id, name: this.name}, 'jwtSecret', {expiresIn: '30d'});
+}
 
 module.exports = mongoose.model('User', UserSchema)
